1308a7ba44568ec447e9ddce6f3403dce242d993
Viet Nguyen <vietjul@gmail.com>
2017-12-14 18:38:08 -0500

    update. cannot populate notes

--- routes/routes.js
+++ routes/routes.js
@@ -119,11 +119,9 @@ module.exports = (app)=>{
         db.Article.findById(id)
         .populate('note')
         .then(function(dbArticle) {
-          // If we were able to successfully find an Article with the given id, send it back to the client
           res.json(dbArticle);
         })
         .catch(function(err) {
-          // If an error occurred, send it to the client
           res.json(err);
         });
     });
